f420cb51f80a409a5d863bd3d8a0b015536527ed
Yiming Zong <yzong@cmu.edu>
2014-10-30 23:21:12 -0400

    Add route for error pages.

--- config/routes.rb
+++ config/routes.rb
@@ -134,4 +134,9 @@ Autolab3::Application.routes.draw do
     end
 
   end
+
+  # Handle error pages
+  match '/404', to: 'errors#file_not_found', via: :all
+  match '/422', to: 'errors#unprocessable', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
 end
